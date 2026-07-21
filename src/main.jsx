9873734b208a29e6fcd5bdc43a45d87798a42689
Pawan Pandey <pandeypawan686800@gmail.com>
2026-07-21 13:46:12 +0530

    initital

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Suroor">
       <App />
     </BrowserRouter>
   </React.StrictMode>
